a2f62f5ffb7de9d2a695056b8fe993b0b4058283
Ashin George <asg043@ucsd.edu>
2018-02-17 18:26:11 -0800

    If else for non multiples of 32

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -6,20 +6,23 @@
 using namespace std;
 #define TW 32
 __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
-//
-//	int I =  blockIdx.y*blockDim.y + threadIdx.y;
-//	int J =  blockIdx.x*blockDim.x + threadIdx.x;
-//
-//	if((I < N) && (J < N)){
-//		_DOUBLE_ _c = 0;
-//		for (unsigned int k = 0; k < N; k++) {
-//			_DOUBLE_ a = A[I * N + k];
-//			_DOUBLE_ b = B[k * N + J];
-//			_c += a * b;
-//		}
-//		C[I * N + J] = _c;
-//	}
-//
+	if(N%TW){
+
+	int I =  blockIdx.y*blockDim.y + threadIdx.y;
+	int J =  blockIdx.x*blockDim.x + threadIdx.x;
+
+	if((I < N) && (J < N)){
+		_DOUBLE_ _c = 0;
+		for (unsigned int k = 0; k < N; k++) {
+			_DOUBLE_ a = A[I * N + k];
+			_DOUBLE_ b = B[k * N + J];
+			_c += a * b;
+		}
+		C[I * N + J] = _c;
+	}
+}
+else
+{
 	__shared__ double As[TW][TW], Bs[TW][TW];
 	int ty = threadIdx.y, tx = threadIdx.x;
 	int by = blockIdx.y, bx = blockIdx.x;
