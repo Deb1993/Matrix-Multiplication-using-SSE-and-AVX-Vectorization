85b1e6c30d2f09c4447eeb41812ffffbe5d7321b
Debosmit <dmajumder@ucsd.edu>
2018-02-19 09:51:29 -0800

    Incorporating Ashin's Code for even and odd

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -1,54 +1,53 @@
 // Matrix multiply device code
 #include <assert.h>
 #include <math.h>
-#include <stdio.h>
 #include "utils.h"
 #include "types.h"
 using namespace std;
-#define TW 32 
-
+#define TW 32
+#include <stdio.h>
 __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
-
-if(N%TW != 0) {
-    int I =  blockIdx.y*blockDim.y + threadIdx.y;
-    int J =  blockIdx.x*blockDim.x + threadIdx.x;
-
-    if((I < N) && (J < N)){
-        _DOUBLE_ _c = 0;
-        for (unsigned int k = 0; k < N; k++) {
-            _DOUBLE_ a = A[I * N + k];
-            _DOUBLE_ b = B[k * N + J];
-            _c += a * b;
-        }
-        C[I * N + J] = _c;
-    }
-}
-else {
-__shared__ double AS[TW][TW], BS[TW][TW];
-
-int ty = threadIdx.y;
-int tx = threadIdx.x;
-int bx = blockIdx.x;
-int by = blockIdx.y;
-
-int I = by*TW + ty;
-int J = bx*TW + tx;
-double Cij = 0;
-
-if( (I < N) && (J < N)) {
-	for (int kk = 0 ; kk < N/TW ; kk++) {
-		//printf("Inside kk loop = %d\n",kk);
-		//printf("Inside kk loop = %d\n",kk);
-		AS[ty][tx] = A[I*N + kk*TW + tx];
-		BS[ty][tx] = B[(kk*TW + ty)*N  + J];
+	__shared__ double As[TW][TW], Bs[TW][TW];
+	int ty = threadIdx.y, tx = threadIdx.x;
+	int by = blockIdx.y, bx = blockIdx.x;
+	double Cij = 0;
+	if(N%TW)
+{
+	int I = min(N-1,by*TW + ty); 
+	int J= min(N-1,bx*TW + tx);
+//	for (int kk=0; kk<ceilf(float (N)/TW); kk++)
+	for (int kk=0; kk<(N/TW + int(bool(N%TW))); kk++)
+	{
+		As[ty][tx] = A[I*N + kk*TW+tx];
+		Bs[ty][tx] = B[(kk*TW+ty)*N + J];
 		__syncthreads();
-		for(int k = 0; k < TW ; k++) {
-			Cij += AS[ty][k] * BS[k][tx];
-		//printf("Inside k loop = %d\n",k);
-		 }
+			//for (int k=0; k<TW && k+kk*TW<N; k++)
+			for (int k=0; k<min(TW,N-kk*TW); k++)
+			{
+				Cij+= As[ty][k] * Bs[k][tx];
+			}
+		__syncthreads();
+	}
+		C[I*N + J] = Cij;
+}
+else
+{
+	int I = by*TW + ty; 
+	int J= bx*TW + tx;
+//	for (int kk=0; kk<ceilf(float (N)/TW); kk++)
+	for (int kk=0; kk<N/TW; kk++)
+	{
+		As[ty][tx] = A[I*N + kk*TW+tx];
+		Bs[ty][tx] = B[(kk*TW+ty)*N + J];
+		__syncthreads();
+			//for (int k=0; k<TW && k+kk*TW<N; k++)
+			for (int k=0; k<TW; k++)
+			{
+				Cij+= As[ty][k] * Bs[k][tx];
+			}
 		__syncthreads();
 		}
-	C[I*N + J] = Cij;
+		C[I*N + J] = Cij;
+
 	}
-}
 }
